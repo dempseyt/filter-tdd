79ea92eaec9cbb68a9bf3c39da2f870e33fddf6d
dempseyt <dempsey.thompson@journeyone.com.au>
2024-02-16 10:23:23 +0800

    GREEN: Given a callback function and an array › returns an array

--- src/filter.js
+++ src/filter.js
@@ -2,5 +2,6 @@ function filter(callbackFunction, arrayToFilter){
     for(let i = 0; i < arrayToFilter.length; i++) {
         callbackFunction(arrayToFilter[i])
     }
+    return arrayToFilter
 }
 export default filter
